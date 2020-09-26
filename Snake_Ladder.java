3226a92dbeb29b052a8156aa2dc2099d5abf262e
Abhishek bhagat <abhishekbhagatraw@gmail.com>
2020-09-26 10:31:45 +0530

    [Abhishek] play with a single player at start pos 0

---
+++ Snake_Ladder.java
@@ -0,0 +1,12 @@
+package abc;
+
+public class Snake_Ladder
+{
+	 public static void main(String[] args) 
+	 { 
+		 //Variable 
+		 int startPosition=0; 
+		 System.out.println("Starting position : "+startPosition);
+    } 
+
+}
